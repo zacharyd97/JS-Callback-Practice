30b9a5a108ac8c557194cff2e6f6a27b8715bd41
Zachary Dowell <zdowell97@gmail.com>
2022-05-31 18:14:36 -0400

    second commit

--- index.js
+++ index.js
@@ -60,11 +60,32 @@ document.addEventListener('keydown', function(e){
     if(e.key === 'ArrowDown'){
         direction = 'south'
     }
+    cancelIdleCallback(direction)
 })
 document.addEventListener('keyup', function(e){
     direction = null
+    callback(direction)
 })
 
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
 move(newImage('assets/tree.png')).to(200, 450)
